b93d075e19836fc8c988e4ecac18950600e34d19
NaimeDmts <naime.dmts@gmail.com>
2023-03-03 16:22:53 +0300

    Slider Uygulaması - Dom

--- script.js
+++ script.js
@@ -28,7 +28,14 @@ var models = [
 
 var index = 0;
 var slaytCount = models.length;
-showSlayt(index);
+var interval;
+
+var settings = {
+    duration : '2000',
+    random : false
+};
+
+init(settings);
 
 document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
     index--;
@@ -42,9 +49,45 @@ document.querySelector('.fa-arrow-circle-right').addEventListener('click',functi
     console.log(index);
 });
 
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval);
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings);
+    })
+})
+function init(settings){
+
+    var prev;
+
+   interval = setInterval(function(){
+
+        if(settings.random){
+            do{
+                index = Math.floor(Math.random() * slaytCount);
+            }while(index == prev)
+            prev = index;
+        }
+        else{
+            if(slaytCount == index+1){
+                index = -1;
+            }
+            showSlayt(index);
+            console.log(index);
+            index++;
+
+        }
+        showSlayt(index);
+    },settings.duration)
+}
+
 function showSlayt(i){
     index = i;
-    
+
     if( i < 0 ){
         index = slaytCount-1;
     }
